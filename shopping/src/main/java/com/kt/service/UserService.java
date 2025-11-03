75297e842c9c4f5ac717c904f2e64fff9fbf9657
Seulgi <jusanha17@gmail.com>
2025-11-03 12:11:37 +0900

    feat: SGKT-30 유저 리스트 조회 서비스 개발

--- shopping/src/main/java/com/kt/service/UserService.java
+++ shopping/src/main/java/com/kt/service/UserService.java
@@ -2,21 +2,24 @@ package com.kt.service;
 
 import java.time.LocalDateTime;
 
+import com.kt.dto.CustomPage;
 import com.kt.dto.UserCreateRequest;
 import com.kt.domain.User;
 import com.kt.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
-public class UserService{
+public class UserService {
 	private final UserRepository userRepository;  // ← 생성자 주입(Lombok)
 
 	@Transactional
-	public void create(UserCreateRequest request){
-				System.out.println(request.toString());
+	public void create(UserCreateRequest request) {
+		System.out.println(request.toString());
 		var newUser = new User(
 			userRepository.selectMaxId() + 1,
 			request.loginId(),
@@ -35,13 +38,13 @@ public class UserService{
 	}
 
 	// 기본형은 null 절대 안됨.
-	public boolean isDuplicateLoginId(String loginId){
+	public boolean isDuplicateLoginId(String loginId) {
 		return userRepository.existsByLoginId(loginId);
 	}
 
 	public void changePassword(Long id, String oldPassword, String password) throws Throwable {
 		var user = userRepository.selectById(id)
-			.orElseThrow(()-> new IllegalArgumentException("존재하지 않는 회원입니다."));
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 
 		// 서비스 입장에서는 id 값이 외부에서 들어오느 값이기 때문에 int로 받음
 		// 실제로 db에 유저가 존재하냐?
@@ -49,13 +52,26 @@ public class UserService{
 		// 1234 -> 1234 로 바꾸면 예외처리 이런거
 
 		// 유저를 조회해서 비밀번호가 조회한 비번과 새로운 비번이 같은지?
-		if(!user.getPassword().equals(oldPassword)){
+		if (!user.getPassword().equals(oldPassword)) {
 			throw new IllegalArgumentException("기본 비밀번호가 일치하지 않습니다.");
 		}
 
-		if(oldPassword.equals(password)){
+		if (oldPassword.equals(password)) {
 			throw new IllegalArgumentException("기본 비밀번호와 동일한 비밀번호는 사용할 수 없습니다.");
 		}
 		userRepository.updatePassword(id, password);
+	}
+
+	public CustomPage search(int page, int size) {
+		var pair = userRepository.selectAll(page - 1, size);
+		var pages = (int)Math.ceil((double)pair.getSecond() / size);
+
+		return new CustomPage(
+			pair.getFirst(),
+			size,
+			page,
+			pages,
+			pair.getSecond()
+		);
 	}
 }
