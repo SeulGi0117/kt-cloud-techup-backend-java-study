73bec9bcd466dd2fe34e3a6582e593a436311851
Seulgi <jusanha17@gmail.com>
2025-11-03 09:22:22 +0900

    feat: SGKT-11 중복로그인체크 리포지토리 개발

--- shopping/src/main/java/com/kt/repository/UserRepository.java
+++ shopping/src/main/java/com/kt/repository/UserRepository.java
@@ -51,4 +51,19 @@ public class UserRepository {
 		var maxId = jdbcTemplate.queryForObject(sql, Long.class);
 		return maxId == null ? 0L : maxId;
 	}
+	// 크게 세가지 젇오 아이디 중복 체크 방법
+	// 1 .count해서 0보다 큰지 체크 -> 강사님은 별로 좋아보이진 않음
+	// 	=> db에 만야 ㄱ유저가 3000만명 -> 1번 중복체크 할떄마다 3천개의 데이터를 모두 살펴봐야한다.(full-scan)
+	// 	db에 만약 동시에 여러 요청이 들어온다면 중복체크 후 저장 사이에 끼어들수도 있음
+	//
+	// 2. unique 제약조건 걸어서 예외 처리 -> 별로 좋아보이진 않음
+	// => 유니크 키 에러(DataViolation Exception) 별로 좋아보이진 않음
+	// 3. exists로 존재 여부 체크 -> boolean으로 값 존재 여부를 바로 알 수 있다. 있으면 T 없으면 F
+
+	public boolean existsByLoginId(String loginId) {
+		var sql = "SELECT EXISTS(SELECT id FROM MEMBER WHERE loginId = ?)";
+		return Boolean.TRUE.equals(jdbcTemplate.queryForObject(sql, Boolean.class, loginId));
+		// return jdbcTemplate.queryForObject(sql, Boolean.class, loginId);
+
+	}
 }
