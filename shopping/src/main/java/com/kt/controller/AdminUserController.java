9f998b33016e2262ad248819cabe4549b618475c
Seulgi <jusanha17@gmail.com>
2025-11-03 12:44:34 +0900

    SGKT-21 어드민 상세 조회 api 개발

--- shopping/src/main/java/com/kt/controller/AdminUserController.java
+++ shopping/src/main/java/com/kt/controller/AdminUserController.java
@@ -2,11 +2,13 @@ package com.kt.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kt.domain.User;
 import com.kt.dto.CustomPage;
 import com.kt.service.UserService;
 
@@ -28,13 +30,19 @@ public class AdminUserController {
 	public CustomPage search(
 		// 안 넣는 경우에 0, 10 이런식으로 하는데 처리하게끔 동작하도록 설정한다. defaultValue를 통해서!
 		@RequestParam(defaultValue = "10") int size,
-		@RequestParam(defaultValue = "1") int page
-		@RequestParam(required = false) String keyword,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(required = false) String keyword
 	) {
 		return userService.search(page, size, keyword);
 	}
 
 	// 유저 상세 조회
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public User detail(@PathVariable Long id) {
+		return userService.detail(id);
+	}
 	// 유저 정보 수정
 	// 유저 삭제
 	// 유저 비번 초기화
