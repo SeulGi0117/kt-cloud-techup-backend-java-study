baf5f5527b5bc43227fd643da25675fa1806f1eb
Seulgi <jusanha17@gmail.com>
2025-11-03 12:40:41 +0900

    feat: SGKT-20 어드민 유저 리스트 조회 api 개발

--- shopping/src/main/java/com/kt/controller/AdminUserController.java
+++ shopping/src/main/java/com/kt/controller/AdminUserController.java
@@ -1,12 +1,39 @@
 package com.kt.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kt.dto.CustomPage;
+import com.kt.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/admin/users")
+@RequiredArgsConstructor
 public class AdminUserController {
+	private final UserService userService;
 	// 유저 리스트 조회
+
+	// GET 요청은 서버가 클라이언트에게 데이털르 전달하거나 조회할 때 사용하는 쿼리스트링
+	// URI 뒤에 붙는 파라미터 형태로 정보를 전달한다
+
+	// ?key=value&page=1&keyword=asdasd
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public CustomPage search(
+		// 안 넣는 경우에 0, 10 이런식으로 하는데 처리하게끔 동작하도록 설정한다. defaultValue를 통해서!
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(defaultValue = "1") int page
+		@RequestParam(required = false) String keyword,
+	) {
+		return userService.search(page, size, keyword);
+	}
+
 	// 유저 상세 조회
 	// 유저 정보 수정
 	// 유저 삭제
